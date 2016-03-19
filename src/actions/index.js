0902c9466da40c145bff6081bb816ecc5218efad
Joe Coleman <phantom42@gmail.com>
2016-03-19 14:57:45 -0400

    lecture 56 set up axios and api call to openweather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'afb2fc6383d703631c930b71b3212338';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`; // backticks = es6 template strings
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
