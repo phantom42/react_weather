9660868ffbd29ac2314d4928133b22f9e80aa1d3
Joe Coleman <phantom42@gmail.com>
2016-04-01 09:16:56 -0400

    wip

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,11 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
 	//console.log('action received', action);
+	switch (action.type) {
+		case FETCH_WEATHER: 
+			//return state.concat([ action.payload.data ]);
+			return [ action.payload.data, ...state]; // es6 concat
+	}
 	return state
 }
